c7c2e5faec81a8bce40c97fe49569b66271727e5
varshanraj <117526984+varshanhack@users.noreply.github.com>
2023-08-03 16:18:30 +0530

    Add files via upload

---
+++ arm.py
@@ -0,0 +1,18 @@
+#Armstrong number
+import sys,subprocess
+while True:
+    l=[]
+    n=input("Enter the number ==> ")
+    ln=len(n)
+    print("")
+    for i in n:
+        no=int(i)**ln
+        l.append(no)
+    lsum=sum(l)
+    if int(n)==lsum:
+        print("The number",n,"is an Armstrong number")
+    else:
+        print("The number",n,"is not an Armstrong number")
+    print("")
+    input("Press any key to continue...")
+    subprocess.run("cls",shell=True)
